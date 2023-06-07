a711f68836ea792332d2c2f51951a7a4c1353d4e
ronnimadeira <115476262+ronnimadeira@users.noreply.github.com>
2023-06-07 18:21:34 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -3,20 +3,25 @@ input.onGesture(Gesture.ScreenUp, function () {
     led.plot(2, 2)
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    basic.clearScreen()
-    x += 1
-    led.plot(x, y)
+    if (x < 5) {
+        basic.clearScreen()
+        x += -1
+        led.plot(x, y)
+    } else {
+        x = -4
+    }
 })
 input.onGesture(Gesture.TiltRight, function () {
-    basic.clearScreen()
-    x += -1
-    led.plot(x, y)
+    if (x < 5) {
+        basic.clearScreen()
+        x += 1
+        led.plot(x, y)
+    } else {
+        x = 4
+    }
 })
 let y = 0
 let x = 0
 x = 2
 y = 2
 led.plot(x, y)
-basic.forever(function () {
-	
-})
